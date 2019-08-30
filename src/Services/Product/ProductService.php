3944030c93a306dad44722986e382de8f3f66b73
Justin Marshall <j.marshall@revers.io>
2019-08-30 14:52:36 +0200

    Last validations

--- src/Services/Product/ProductService.php
+++ src/Services/Product/ProductService.php
@@ -74,34 +74,6 @@ class ProductService
                     $images[0]['id_image']
                 );
             }
-
-            $productsArray[] = [
-                "brandId" => $brandId,
-                "modelTypeId" => $categoryId,
-                "sKU" => $product->reference,
-                "label" => $product->name[$language],
-                "eANs" => [
-                    $product->ean13,
-                ],
-                "dimension" => [
-                    "lengthInCm" => (float)$product->depth,
-                    "widthInCm" => (float)$product->width,
-                    "heightInCm" => (float)$product->height,
-                ],
-                "photoUrl" => $imageUrl,
-                "additionalInformation" => [
-                    "isReturnable" => true,
-                    "isRepairable" => true,
-                    "isTransportable" => true,
-                    "isSerializable" => false,
-                    "isOnSiteInterventionPossible" => false,
-                    "isCumbersome" => false
-                ],
-                "state" => 'new',
-                "weight" => (float)$product->weight,
-                "isLowValue" => true,
-                "id_product" => $product->id,
-            ];
         }
 
         $productInfoArray = [
